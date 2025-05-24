ef70285b7b6c6c782b500b141cf7d64781c626ab
Joshi-Anish <anish.221450@ncit.edu.np>
2025-05-24 08:25:19 +0545

    changed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Worlds"}
 @app.get("/greet/{name}")
 async def greet(name: str) -> dict:
     return {"message": f"Hello, {name}!"}
