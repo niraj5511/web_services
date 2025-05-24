5180610867f05bc3e9e9345b87a86f8e0670d19b
Daya Shankar Adhikari <dayashankaradhikari@gmail.com>
2025-05-24 07:28:51 +0545

    initial file

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/",status_code=201)
+def read_root():
+    return {"Hello": "World"}
+
+class User(BaseModel):
+    username: str="Niraj"
+    email: str="niraj.221315@ncit.edu.np"
+
+# POST endpoint
+@app.post("/create-user/")
+def create_user(user: User):
+    return {
+    "message": f"User {user.username} with email {user.email} created successfully!"
+}
